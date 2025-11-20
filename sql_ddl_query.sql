f960539bd8cc338c075decc6d747664ac508bbc8
Sara Ammourh <saraammourh814@gmail.com>
2025-11-20 23:06:13 +0300

    Modified DDL Commands

--- sql_ddl_query.sql
+++ sql_ddl_query.sql
@@ -5,27 +5,27 @@ CREATE TABLE users
   email VARCHAR(254) NOT NULL,
   preferred_theme VARCHAR(15) NOT NULL,
   created_at DATE NOT NULL,
-  phone_number VARCHAR(20) NOT NULL,
-  saved_vis_num INT NOT NULL,
+  phone_number VARCHAR(20),
+  saved_vis_num INT NOT NULL DEFAULT (0),
   PRIMARY KEY (user_id)
 );
 
 CREATE TABLE user_auth
 (
-  password VARCHAR NOT NULL,
-  token VARCHAR NOT NULL,
+  password VARCHAR(MAX) NOT NULL,
+  token VARCHAR(MAX) NOT NULL,
   token_created_at DATE NOT NULL,
   user_id INT NOT NULL,
-  PRIMARY KEY (password),
+  PRIMARY KEY (password, user_id),
   FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE user_post
 (
+  id INT NOT NULL,
   operation_type VARCHAR(30) NOT NULL,
   created_at DATE NOT NULL,
   status VARCHAR(15) NOT NULL,
-  id INT NOT NULL,
   user_id INT NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES users(user_id)
@@ -33,7 +33,7 @@ CREATE TABLE user_post
 
 CREATE TABLE users_saved_visuals
 (
-  saved_visual  NOT NULL,
+  saved_visual JSON NOT NULL,
   type VARCHAR(30) NOT NULL,
   updated_at DATE NOT NULL,
   user_id INT NOT NULL,
@@ -45,8 +45,8 @@ CREATE TABLE label_correction
 (
   image_path VARCHAR(256) NOT NULL,
   data_structure_type VARCHAR(30) NOT NULL,
-  wrong_label ,
-  correct_label  NOT NULL,
+  wrong_label JSON,
+  correct_label JSON NOT NULL,
   created_at DATE NOT NULL,
   user_id INT NOT NULL,
   PRIMARY KEY (image_path),
