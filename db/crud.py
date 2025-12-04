7a99f071abd4a2dcad9a415c6da4c704c3754abf
Sara Ammourh <saraammourh814@gmail.com>
2025-12-04 22:59:36 +0300

    Added CRUD for UserAuth, UserPost, and UsersSavedVisuals

--- db/crud.py
+++ db/crud.py
@@ -2,8 +2,10 @@ from datetime import date
 from typing import Optional, List
 from sqlmodel import select, Session
 from db.config import get_session
-from db.models import User
+from db.models import User, UserAuth, UserPost, UsersSavedVisuals
 
+
+# User CRUD
 
 def create_user(
     username: str,
@@ -40,8 +42,10 @@ def get_user_by_id(
 ) -> Optional[User]:
     return session.exec(select(User).where(User.user_id == user_id)).first()
 
+
 def get_all_users(session: Session = next(get_session())) -> List[User]:
     return session.exec(select(User)).all()
+
 
 def update_user(user_id: int, data: dict, session: Session = next(get_session())) -> Optional[User]:
     user = session.exec(select(User).where(User.user_id == user_id)).first()
@@ -53,3 +57,160 @@ def update_user(user_id: int, data: dict, session: Session = next(get_session())
         session.refresh(user)
         return user
     return None
+
+
+# UserAuth CRUD
+
+def create_user_auth(
+        password: str,
+        token: str,
+        user_id: int,
+        session: Session = next(get_session())
+) -> UserAuth:
+    new_user_auth = UserAuth(
+        user_id=user_id,
+        password=password,
+        token=token,
+        token_created_at=date.today(),
+    )
+
+    session.add(new_user_auth)
+    session.commit()
+    session.refresh(new_user_auth)
+    return new_user_auth
+
+
+def remove_user_auth(user_id: int, session: Session = next(get_session())) -> bool:
+    user_auth = session.exec(select(UserAuth).where(UserAuth.user_id == user_id)).first()
+    if user_auth:
+        session.delete(user_auth)
+        session.commit()
+        return True
+    return False
+
+
+def get_user_auth_by_id(
+    user_id: int, session: Session = next(get_session())
+) -> Optional[UserAuth]:
+    return session.exec(select(UserAuth).where(UserAuth.user_id == user_id)).first()
+
+
+def update_user_auth(user_id: int, data: dict, session: Session = next(get_session())) -> Optional[UserAuth]:
+    user_auth = session.exec(select(UserAuth).where(UserAuth.user_id == user_id)).first()
+    if user_auth:
+        for key, value in data.items():
+            setattr(user_auth, key, value)
+        session.add(user_auth)
+        session.commit()
+        session.refresh(user_auth)
+        return user_auth
+    return None
+
+
+# UserPost CRUD
+
+def create_user_post(
+        operation_type: str,
+        status: str,
+        user_id: int,
+        session: Session = next(get_session())
+) -> UserPost:
+    new_user_post = UserPost(
+        operation_type=operation_type,
+        created_at=date.today(),
+        status=status,
+        user_id=user_id,
+    )
+
+    session.add(new_user_post)
+    session.commit()
+    session.refresh(new_user_post)
+    return new_user_post
+
+
+def remove_user_post(id: int, session: Session = next(get_session())) -> bool:
+    user_post = session.exec(select(UserPost).where(UserPost.id == id)).first()
+    if user_post:
+        session.delete(user_post)
+        session.commit()
+        return True
+    return False
+
+
+def get_user_post_by_id(id: int, session: Session = next(get_session())) -> Optional[UserPost]:
+    return session.exec(select(UserPost).where(UserPost.id == id)).first()
+
+
+def get_all_user_posts(session: Session = next(get_session())) -> List[UserPost]:
+    return session.exec(select(UserPost)).all()
+
+
+# UsersSavedVisuals CRUD
+
+def create_saved_visual (
+        saved_visual: str,
+        type: str,
+        user_id: int,
+        session: Session = next(get_session())
+) -> UsersSavedVisuals:
+    new_saved_visual = UsersSavedVisuals(
+        saved_visual=saved_visual,
+        type=type,
+        user_id=user_id,
+        updated_at=date.today(),
+    )
+
+    session.add(new_saved_visual)
+    session.commit()
+    session.refresh(new_saved_visual)
+    return new_saved_visual
+
+
+def remove_saved_visual(id: int, session: Session = next(get_session())) -> bool:
+    saved_visual = session.exec(select(UsersSavedVisuals).where(UsersSavedVisuals.id == id)).first()
+    if saved_visual:
+        session.delete(saved_visual)
+        session.commit()
+        return True
+    return False
+
+
+def get_saved_visual_by_id(id: int, session: Session = next(get_session())
+) -> Optional[UsersSavedVisuals]:
+    return session.exec(select(UsersSavedVisuals).where(UsersSavedVisuals.id == id)).first()
+
+
+def get_all_saved_visuals(session: Session = next(get_session())) -> List[UsersSavedVisuals]:
+    return session.exec(select(UsersSavedVisuals)).all()
+
+
+def get_saved_visual_by_user_id(
+        user_id: int, session: Session = next(get_session())
+) -> List[UsersSavedVisuals]:
+    return session.exec(select(UsersSavedVisuals.user_id == user_id)).all()
+
+
+def update_saved_visual(id: int, data: dict, session: Session = next(get_session())
+                        ) -> Optional[UsersSavedVisuals]:
+    saved_visual = session.exec(select(UsersSavedVisuals).where(UsersSavedVisuals.id == id)).first()
+    if saved_visual:
+        for key, value in data.items():
+            setattr(saved_visual, key, value)
+        setattr(saved_visual, 'updated_at', date.today())
+        session.add(saved_visual)
+        session.commit()
+        session.refresh(saved_visual)
+        return saved_visual
+    return None
+
+
+# LabelCorrection CRUD
+
+# def create_label_correction(
+#         image_path: str,
+#         data_structure_type: str,
+#         wrong_label: Optional[str] = None,
+#         correct_label: str,
+#         user_id: int,
+#         session: Session = next(get_session())
+# ) ->
